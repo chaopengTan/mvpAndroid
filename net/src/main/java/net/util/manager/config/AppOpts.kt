f5dfcb58e619b84964abcf75a8fe41bff94b1eb1
stones.tan <stones.tan@shijigroup.com>
2020-11-11 10:14:34 +0800

    first push

---
+++ net/src/main/java/net/util/manager/config/AppOpts.kt
@@ -0,0 +1,21 @@
+package net.util.manager.config
+
+import android.content.Context
+
+class AppOpts {
+    companion object{
+        private var context: Context? =null
+        lateinit var TAG:String
+        fun initContent(context: Context){
+            this.context = context
+            TAG = getContenxt().packageName + "_TAG"
+        }
+
+        fun getContenxt():Context{
+            return this!!.context!!
+        }
+
+
+
+    }
+}
